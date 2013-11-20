b2f79c9622584c7461a59e05fb6a3a9e1ef62785
2wavy <tyler.jenkins@bison.howard.edu>
2013-11-19 20:48:49 -0500

    bsearch.pywave

--- projects/bsearch/GroupWAVBsearch.py
+++ projects/bsearch/GroupWAVBsearch.py
@@ -4,3 +4,25 @@
 #Eboni
 #Ashanti
 #Tyler
+
+waveList = ['Too', 'Wavy', 'For', 'These', 'Jawns', 'Cause', 'Im', 'From', 'Atlantis']
+
+def bsearch():
+	lo = 0
+	hi = len(waveList)- 1
+	target = False
+	destination = waveList[5]
+	while lo < hi and target == False:
+		mid = ((lo + hi)//2)
+		if waveList[mid] < destination:
+			lo = mid + 1
+		elif waveList[mid] > destination:
+			hi = mid - 1
+		else:
+			target = True
+	if target:
+		print 'Success'
+	else:
+		print '-1'		
+
+bsearch ()
