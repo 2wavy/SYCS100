93fce93db04c144c26c865fb094a46199d509601
2wavy <tyler.jenkins@bison.howard.edu>
2013-11-21 15:53:50 -0500

    WAVbsearch.py (modified so you can mortify)
    I had to finesse a brand new code because the class website would not accept my previous binary search. I just turned this one in and received a perfect score, hopefully we have reached the destination. #NWTS

--- projects/bsearch/GroupWAVBsearch.py
+++ projects/bsearch/GroupWAVBsearch.py
@@ -4,3 +4,23 @@
 #Eboni
 #Ashanti
 #Tyler
+waveList = [3,4,6,8,9]
+
+
+def binarySearch(MyPurpose, waveList):
+    Discovery = False
+    bottom = 0
+    top = len(waveList) - 1
+    if bottom == top:
+        return -1
+    while bottom<=top and not Discovery:
+        mid = (bottom+top)//2
+        if waveList[mid]==MyPurpose:
+            Discovery = True
+        elif waveList[mid] < MyPurpose:
+            bottom = mid + 1
+        else:
+            top = mid - 1
+    return Discovery
+
+    print binarySearch(MyPurpose, waveList)
